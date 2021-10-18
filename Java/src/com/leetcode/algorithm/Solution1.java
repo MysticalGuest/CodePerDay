6912b1a5bed43883dee240d6296f101a24abff52
MysticalGuest <1317148109@qq.com>
2021-10-18 19:57:46 +0800

    Update Solution1.java

--- Java/src/com/leetcode/algorithm/Solution1.java
+++ Java/src/com/leetcode/algorithm/Solution1.java
@@ -15,13 +15,13 @@ package com.leetcode.algorithm;
 /**
  * Definition for singly-linked list.
  */
-class ListNode {
-	int val;
-	ListNode next;
-	ListNode() {}
-	ListNode(int val) { this.val = val; }
-	ListNode(int val, ListNode next) { this.val = val; this.next = next; }
-}
+//class ListNode {
+//	int val;
+//	ListNode next;
+//	ListNode() {}
+//	ListNode(int val) { this.val = val; }
+//	ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+//}
 
 public class Solution1 {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
